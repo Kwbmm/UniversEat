b9ea8f83f4b82d7c108c015fcad5c6c5ffcac704
Giovanni Aquila <s231718@studenti.polito.it>
2016-04-04 12:18:47 +0200

    create file datastructures

---
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/Course.java
@@ -0,0 +1,8 @@
+package it.polito.mad.groupFive.restaurantcode.datastructures;
+
+/**
+ * Created by Giovanni on 04/04/16.
+ */
+public class Course
+{
+}
---
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/User.java
@@ -0,0 +1,7 @@
+package it.polito.mad.groupFive.restaurantcode.datastructures;
+
+/**
+ * Created by Giovanni on 04/04/16.
+ */
+public class User {
+}
