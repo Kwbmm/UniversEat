6f23b3623affab78d8249ced9d8bbf3e0476e613
Giovanni Aquila <s231718@studenti.polito.it>
2016-04-04 14:31:17 +0200

    add getter and setter class restaurant and user

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/Restaurant.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/Restaurant.java
@@ -19,6 +19,203 @@ public class Restaurant {
     private ArrayList<Menu> menus;
     private String description;
     private ArrayList<Order> orders;
+    private float xcoord;
+    private float ycoord;
 
+    public Restaurant(){
+
+
+    }
+
+    /**
+     * @return string: name of restaurant
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name of restaurant
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return address of restaurant
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     *
+     * @param address of restaurant
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     *
+     * @return State of restaurant
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     *
+     * @param state: the State of restaurant
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     *
+     * @return city of Restaurant
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     *
+     * @param city of restaurant
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     *
+     * @return byte of restaurant's image
+     */
+    public byte[] getImage() {
+        return image;
+    }
+
+    /**
+     *
+     * @param image the byte of image of restaurant
+     */
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    /**
+     *
+     * @return the latitude coordinate of restaurant
+     */
+    public float getXcoord() {
+        return xcoord;
+    }
+
+    /**
+     *
+     * @param xcoord: the latitude coordinate of restaurant
+     */
+    public void setXcoord(float xcoord) {
+        this.xcoord = xcoord;
+    }
+
+    /**
+     *
+     * @return the longitude coordinate of restaurant
+     */
+    public float getYcoord() {
+        return ycoord;
+    }
+
+    /**
+     *
+     * @param ycoord: the longitude coordinate of restaurant
+     */
+    public void setYcoord(float ycoord) {
+        this.ycoord = ycoord;
+    }
+
+    /**
+     *
+     * @return the description of restaurant
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     *
+     * @param description of the restaurant
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     *
+     * @return the id of restaurant
+     */
+    public int getRid() {
+        return rid;
+    }
+
+    /**
+     *
+     * @param rid: the id of restaurant
+     */
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+
+    /**
+     *
+     * @return the rating of restaurant
+     */
+    public float getRating() {
+        return rating;
+    }
+
+    /**
+     *
+     * @param rating of restaurant
+     */
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    /**
+     *
+     * @return the arraylist of restaurant's menu
+     */
+    public ArrayList<Menu> getMenus() {
+        return menus;
+    }
+
+    /**
+     *
+     * @param menus: the arraylist of restaurant's menu
+     */
+    public void setMenus(ArrayList<Menu> menus) {
+        this.menus = menus;
+    }
+
+    /**
+     *
+     * @return the arraylist of restaurant's orders
+     */
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    /**
+     *
+     * @param orders:the arraylist of restaurant's orders
+     */
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
 
 }
--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/User.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/User.java
@@ -16,7 +16,156 @@ public class User {
     private String address;
     private byte[] image;
     private String nickname;
-    private ArrayList<Restaurant> prefers;
+    private ArrayList<Restaurant> favorites;
     private boolean restaurantowner;
     private Restaurant restaurant;
+
+    public User() {
+
+    }
+
+    /**
+     *
+     * @return the name of user
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name of the user
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return the surname of user
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     *
+     * @param surname of the user
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     *
+     * @return the address of user
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     *
+     * @param address of the user
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     *
+     * @return the nickname of the user
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     *
+     * @param nickname of the user
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     *
+     * @return byte[] of user's image
+     */
+    public byte[] getImage() {
+        return image;
+    }
+
+    /**
+     *
+     * @param image of user
+     */
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    /**
+     *
+     * @return yes if the user is the restaurant owner
+     */
+    public boolean isRestaurantowner() {
+        return restaurantowner;
+    }
+
+    /**
+     *
+     * @param restaurantowner if is or is not a restaurant owner
+     */
+    public void setRestaurantowner(boolean restaurantowner) {
+        this.restaurantowner = restaurantowner;
+    }
+
+    /**
+     *
+     * @return the id of user
+     */
+    public int getUid() {
+        return uid;
+    }
+
+    /**
+     *
+     * @param uid the id of user
+     */
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    /**
+     *
+     * @return the arraylist of user's favorite restaurants 
+     */
+    public ArrayList<Restaurant> getFavorites() {
+        return favorites;
+    }
+
+    /**
+     *
+     * @param favorite: the arraylist of user's favorite restaurants
+     */
+    public void setFavorites(ArrayList<Restaurant> favorite) {
+        this.favorites = favorite;
+    }
+
+    /**
+     *
+     * @return the restaurant of the user (restaurant manager)
+     */
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    /**
+     *
+     * @param restaurant: the restaurant of the user (restaurant manager)
+     */
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
 }
