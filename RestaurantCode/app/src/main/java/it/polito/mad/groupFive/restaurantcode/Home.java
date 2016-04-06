e737d9fc408528c9626480ddde8b5599bbbdc8ca
Federico Merlin <federico.merlin@gmail.com>
2016-04-06 02:08:59 +0200

    Global navigation drawer + Action bar

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Home.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Home.java
@@ -1,13 +1,27 @@
 package it.polito.mad.groupFive.restaurantcode;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class Home extends NavigationDrawer {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        //Every activity should extend navigation drawer activity, and set content view must be called before super.onCreate() to work
         setContentView(R.layout.activity_home);
-    }
-}
+        super.onCreate(savedInstanceState);
+
+
+    }}
---
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/NavigationDrawer.java
@@ -0,0 +1,82 @@
+package it.polito.mad.groupFive.restaurantcode;
+
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+public class NavigationDrawer extends AppCompatActivity {
+    ActionBarDrawerToggle mDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // not a real activity, it's used to extend toolbar and navigation drawer to all activity created
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_navigation_drawer);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        {
+
+            public void onDrawerClosed(View view)
+            {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+
+            }
+
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        drawerLayout.addDrawerListener(mDrawerToggle);
+
+
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        return super.onPrepareOptionsMenu(menu);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        Log.v("click", "here");
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+}
