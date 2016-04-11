04765501c4597c088ed3b4ae5f317c626019ae58
Federico Merlin <federico.merlin@gmail.com>
2016-04-11 19:33:54 +0200

    How to set up a fragment inside NavigationDrawer

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Create_menu.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Create_menu.java
@@ -1,13 +1,34 @@
 package it.polito.mad.groupFive.restaurantcode;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.FrameLayout;
 
-public class Create_menu extends NavigationDrawer {
+public class Create_menu extends NavigationDrawer implements Create_menu_frag.OnFragmentInteractionListener
+         {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_menu);
+        FrameLayout mlay= (FrameLayout) findViewById(R.id.frame);
+        mlay.inflate(this, R.layout.activity_create_menu, mlay);
+        Create_menu_frag fragment= new Create_menu_frag();
+        getSupportFragmentManager().beginTransaction().add(R.id.acm_1,fragment).commit();
+
+
+
+
+
+
     }
-}
+
+             @Override
+             public void onFragmentInteraction(Uri uri) {
+
+             }
+         }
--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/NavigationDrawer.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/NavigationDrawer.java
@@ -2,6 +2,7 @@ package it.polito.mad.groupFive.restaurantcode;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +17,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 
-public class NavigationDrawer extends AppCompatActivity {
+public class NavigationDrawer extends AppCompatActivity{
     ActionBarDrawerToggle mDrawerToggle;
     ListView dList;
     @Override
