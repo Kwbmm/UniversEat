39f42fa2311f4702c20517330e86316204beeeb7
Federico Merlin <federico.merlin@gmail.com>
2016-04-12 11:25:56 +0200

    Resturant View with data integration

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Restaurant_management.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Restaurant_management.java
@@ -1,6 +1,7 @@
 package it.polito.mad.groupFive.restaurantcode;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -39,16 +42,35 @@ public class Restaurant_management extends NavigationDrawer {
             editor.commit();
 
     */
-
-    User user;
-    Restaurant restaurant;
+    private User user;
+    private Restaurant restaurant;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        user=new User();
+        try {
+            user=new User(this,2,2);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (RestaurantException e) {
+            e.printStackTrace();
+        } catch (UserException e) {
+            e.printStackTrace();
+        }
+        restaurant=user.getRestaurant();
+        sharedPreferences=this.getSharedPreferences(getString(R.string.user_pref),this.MODE_PRIVATE);
         super.onCreate(savedInstanceState);
         FrameLayout mlay= (FrameLayout) findViewById(R.id.frame);
         mlay.inflate(this, R.layout.restaurant_view_edit, mlay);
         showresturant();
+        RelativeLayout rl=(RelativeLayout) findViewById(R.id.rvef_rectangle);
+        rl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("intent","menu");
+            }
+        });
 
 
 
@@ -60,13 +82,19 @@ public class Restaurant_management extends NavigationDrawer {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.toolbar_add, menu);
+
+        MenuItem item=menu.findItem(R.id.add_ab);
+        if ((sharedPreferences.getBoolean("rest",false))!=false){
+            item.setEnabled(false);
+            item.setVisible(false);
+        }
         return true;
     }
 
     private boolean showresturant() {
-        int uid=1,rid;
+        int uid=2,rid=2;
         SharedPreferences sharedPreferences=this.getSharedPreferences(getString(R.string.user_pref),this.MODE_PRIVATE);
-        if ((rid=sharedPreferences.getInt("rid",-1))!=-1){
+        if ((rid=sharedPreferences.getInt("rid",-1))!=-1&&(sharedPreferences.getBoolean("rest",false))){
             FrameLayout rview= (FrameLayout) findViewById(R.id.fl_redit);
             rview.inflate(this,R.layout.resturant_view_edit_fragment,rview);
             ImageButton modify = (ImageButton) findViewById(R.id.rved_modify);
@@ -77,18 +105,7 @@ public class Restaurant_management extends NavigationDrawer {
                 }
             });
 
-            try {
-                user=new User(this,uid,rid);
-                restaurant=new Restaurant(this,1,1);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (RestaurantException e) {
-                e.printStackTrace();
-            } catch (UserException e) {
-                e.printStackTrace();
-            }
+
             getResaurantdata();
             return true;
         }
@@ -97,26 +114,34 @@ public class Restaurant_management extends NavigationDrawer {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences.Editor editor= sharedPreferences.edit();
         if(item.getItemId()==R.id.add_ab){
-            SharedPreferences sharedPreferences=this.getSharedPreferences(getString(R.string.user_pref),this.MODE_PRIVATE);
-            SharedPreferences.Editor editor= sharedPreferences.edit();
-            editor.putInt("rid",1);
-            editor.commit();
+if ((sharedPreferences.getBoolean("rest",false))==false){
+    Log.v("intent","newRest");
             showresturant();
+            item.setEnabled(false);
+            item.setVisible(false);
 
-        }
+        }}
         return super.onOptionsItemSelected(item);
     }
 
     private boolean getResaurantdata(){
-        TextView textView= (TextView)findViewById(R.id.rs_name);
+
+        TextView rname= (TextView)findViewById(R.id.rs_name);
+        TextView raddress= (TextView)findViewById(R.id.rwef_address);
+        RatingBar rbar=(RatingBar)findViewById(R.id.rwef_rate);
         try {
             restaurant.getData();
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        textView.setText(restaurant.getName());
+        rname.setText(restaurant.getName());
+        raddress.setText(restaurant.getAddress());
+        rbar.setRating(restaurant.getRating());
+
+
         return true;
 
     }
