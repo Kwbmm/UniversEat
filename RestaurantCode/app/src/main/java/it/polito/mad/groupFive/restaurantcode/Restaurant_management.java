ee8b5dbef88be5791aca9d811c592f5207841bee
Federico Merlin <federico.merlin@gmail.com>
2016-04-14 22:39:24 +0200

    Fix after merge

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Restaurant_management.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Restaurant_management.java
@@ -1,10 +1,7 @@
 package it.polito.mad.groupFive.restaurantcode;
 
-<<<<<<< HEAD
 import android.app.Activity;
 import android.content.ClipData;
-=======
->>>>>>> 2aa254ca7ed9e394019313b15db1f9d1c9428cea
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,14 +11,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-<<<<<<< HEAD
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-=======
->>>>>>> 2aa254ca7ed9e394019313b15db1f9d1c9428cea
 
 import org.json.JSONException;
 
@@ -50,18 +44,10 @@ public class Restaurant_management extends NavigationDrawer {
             editor.commit();
 
     */
-<<<<<<< HEAD
+    private static final int CREATE_RESTAURANT = 1;
     private User user;
     private Restaurant restaurant;
     private SharedPreferences sharedPreferences;
-=======
-
-    User user;
-
-    //Intent request codes
-    private static final int CREATE_RESTAURANT = 1;
-
->>>>>>> 2aa254ca7ed9e394019313b15db1f9d1c9428cea
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         sharedPreferences=this.getSharedPreferences(getString(R.string.user_pref),this.MODE_PRIVATE);
@@ -69,15 +55,12 @@ public class Restaurant_management extends NavigationDrawer {
         FrameLayout mlay= (FrameLayout) findViewById(R.id.frame);
         mlay.inflate(this, R.layout.restaurant_view_edit, mlay);
         showresturant();
-<<<<<<< HEAD
 
 
 
 
 
 
-=======
->>>>>>> 2aa254ca7ed9e394019313b15db1f9d1c9428cea
     }
 
     @Override
@@ -141,18 +124,12 @@ public class Restaurant_management extends NavigationDrawer {
     public boolean onOptionsItemSelected(MenuItem item) {
         SharedPreferences.Editor editor= sharedPreferences.edit();
         if(item.getItemId()==R.id.add_ab){
-<<<<<<< HEAD
             Log.v("intent","newRest");
-
-=======
-            SharedPreferences sharedPreferences=this.getSharedPreferences(getString(R.string.user_pref),this.MODE_PRIVATE);
-            SharedPreferences.Editor editor= sharedPreferences.edit();
+            Intent intent=new Intent(getApplicationContext(),CreateRestaurant.class);
             editor.putInt("uid",1);
             editor.commit();
-            Intent i = new Intent(getApplicationContext(),CreateRestaurant.class);
-            startActivityForResult(i,CREATE_RESTAURANT);
-//            showresturant();
->>>>>>> 2aa254ca7ed9e394019313b15db1f9d1c9428cea
+            startActivityForResult(intent,CREATE_RESTAURANT);
+
         }
         return super.onOptionsItemSelected(item);
     }
