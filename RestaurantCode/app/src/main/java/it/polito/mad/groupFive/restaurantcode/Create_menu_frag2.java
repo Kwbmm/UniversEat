8c2d243ab9f258907811b02e065b2eaa11e3e250
Federico Merlin <federico.merlin@gmail.com>
2016-04-14 02:01:48 +0200

    Menu creation fragment 2 layout implemented

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Create_menu_frag2.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Create_menu_frag2.java
@@ -2,6 +2,7 @@ package it.polito.mad.groupFive.restaurantcode;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.VoiceInteractor;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -18,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,24 +27,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.json.JSONException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.Map;
+import java.util.Set;
 
+import it.polito.mad.groupFive.restaurantcode.datastructures.Course;
 import it.polito.mad.groupFive.restaurantcode.datastructures.Menu;
 import it.polito.mad.groupFive.restaurantcode.datastructures.Restaurant;
+import it.polito.mad.groupFive.restaurantcode.datastructures.exceptions.CourseException;
 import it.polito.mad.groupFive.restaurantcode.libs.RealPathUtil;
 
 public class Create_menu_frag2 extends Fragment {
+    int dimension=3;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -70,10 +84,14 @@ public class Create_menu_frag2 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final String METHOD_NAME = this.getClass().getName()+"onCreate";
+        final String METHOD_NAME = this.getClass().getName() + "onCreate";
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
+
+
         }
 
     }
@@ -81,7 +99,7 @@ public class Create_menu_frag2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final String METHOD_NAME = this.getClass().getName()+" - onCreateView";
+        final String METHOD_NAME = this.getClass().getName() + " - onCreateView";
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_create_menu_2, container, false);
 
@@ -90,14 +108,25 @@ public class Create_menu_frag2 extends Fragment {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(METHOD_NAME,"Press Next: OK");
+                Log.d(METHOD_NAME, "Press Next: OK");
 
 
                 //TODO passare type, immagine, recuperare restaurant id, creare oggetto menu, lanciare nuova activity
             }
         });
+        ArrayList<Option> options =new ArrayList<>();
+        for (int i=0;i<dimension;i++){
+            Option opt=new Option();
+            options.add(opt);
+        }
+
+        MenuCourse adp=new MenuCourse(container.getContext(),options);
+        ListView lwcm = (ListView) v.findViewById(R.id.lwch_2_1);
+        lwcm.setAdapter(adp);
+        Log.v("dim",adp.getCount()+"num");
         return v;
     }
+
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
@@ -121,8 +150,84 @@ public class Create_menu_frag2 extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+
     }
+
+    public class MenuCourse extends BaseAdapter {
+        Context context;
+        ArrayList<Option> options;
+
+        public MenuCourse(ArrayList<Option> options){
+            this.options=options;
+        }
+
+
+        public MenuCourse(Context context, ArrayList<Option> options) {
+            this.options=options;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return options.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return options.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return getItem(position).hashCode();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.menu_choice, null);
+            TextView opt_title= (TextView) convertView.findViewById(R.id.twch);
+            opt_title.setText("Option "+(position+1));
+            LinearLayout ll= (LinearLayout) convertView.findViewById(R.id.ll_mc);
+
+
+            for (String str:options.get(position).dishes){
+                LayoutInflater inflater=LayoutInflater.from(context);
+                View child =inflater.inflate(R.layout.row,null);
+                ll.addView(child);
+                TextView textView= (TextView) child.findViewById(R.id.ll_row);
+                textView.setText("- "+str);
+
+
+            }
+            return convertView;
+
+
+        }
+
+
+            }
+
+   private class Option{
+    private ArrayList<String> dishes;
+
+       public ArrayList<String> getDishes() {
+           return dishes;
+       }
+
+       public Option(){
+        dishes=new ArrayList<String>();
+           dishes.add("torta");
+           dishes.add("mela");
+           dishes.add("carota");
+           dishes.add("salame");
+           dishes.add("zucca");
+           dishes.add("pizza");
+
+    }
+   }
 }
+
