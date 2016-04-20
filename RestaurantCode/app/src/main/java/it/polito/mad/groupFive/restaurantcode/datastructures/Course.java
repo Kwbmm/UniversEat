1a7912cf66b293a7ad84a7ef8e0b51b51336a8a1
Marco Ardizzone <kwbmm1990@gmail.com>
2016-04-20 10:35:47 +0200

    Add Javadocs and setImageUriFromString

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/Course.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/Course.java
@@ -31,21 +31,36 @@ public class Course {
     private boolean vegetarian;
     private boolean spicy;
 
+    /**
+     * Create an instance of Course: requires, as parameter, its restaurant object.
+     * The ID of the course is generated automatically.
+     *
+     * @param restaurant The restaurant object whose course belongs to.
+     */
     public Course(Restaurant restaurant){
         this.r = restaurant;
         this.cid = Course.randInt();
     }
 
-    public Course(Restaurant restaurant, int cid, int mid) throws CourseException, MenuException {
+    /**
+     * Create an instance of Course: requires, as parameters, its restaurant object and an integer
+     * positive ID to uniquely identify this object.
+     *
+     * @param restaurant The restaurant object whose course belongs to
+     * @param cid A positive integer unique identifier.
+     * @throws CourseException Thrown if cid is negative.
+     */
+    public Course(Restaurant restaurant, int cid) throws CourseException {
         if(cid < 0)
             throw new CourseException("Course ID must be positive");
         this.cid = cid;
-        if(mid < 0)
-            throw new MenuException("Menu ID must be positive");
-        this.mid = mid;
         this.r = restaurant;
     }
 
+    /**
+     * Generate a random integer in the range [1, Integer.MAX_VALUE]
+     * @return
+     */
     private static int randInt() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
             return ThreadLocalRandom.current().nextInt(1,Integer.MAX_VALUE);
@@ -55,6 +70,12 @@ public class Course {
         }
     }
 
+    /**
+     * Fetch the data corresponding to this Course ID from the JSON file. Fetch operations are
+     * always performed inside the restaurant object, this is just a call to that method.
+     *
+     * @throws RestaurantException If fetch fails inside restaurant object
+     */
     public void getData() throws RestaurantException {
         final String METHOD_NAME = this.getClass().getName()+" - getData";
         this.r.getData();
@@ -66,6 +87,10 @@ public class Course {
         }
     }
 
+    /**
+     * Copy all the data took from the JSON file on this object.
+     * @param d A dummy Course object, on which the JSON data is written to.
+     */
     private void copyData(Course d){
         this.cid = d.getCid();
         this.mid = d.getMid();
@@ -83,65 +108,83 @@ public class Course {
      *
      * @return The Course ID
      */
-    public int getCid(){return this.cid;}
+    public int getCid(){ return this.cid;}
 
+    /**
+     *
+     * @return The ID of the menu whose this course belongs to.
+     */
     public int getMid(){ return this.mid; }
 
     /**
      *
      * @return The name of the course
      */
-    public String getName(){return this.name;}
+    public String getName(){ return this.name; }
 
     /**
      *
      * @return The description of the course
      */
-    public String getDescription(){return this.description;}
+    public String getDescription(){ return this.description; }
 
     /**
      *
      * @return The price of the course
      */
-    public float getPrice(){return this.price;}
+    public float getPrice(){ return this.price; }
 
     /**
      *
      * @return The Uri of the course image
      */
-    public Uri getImageUri(){return this.image;}
+    public Uri getImageUri(){ return this.image; }
 
     /**
      * Returns true if course is gluten-free. False otherwise.
      * @return true or false
      */
-    public boolean isGlutenFree(){ return this.glutenFree;}
+    public boolean isGlutenFree(){ return this.glutenFree; }
 
     /**
      * Returns true if course is vegan. False otherwise.
      * @return true or false
      */
-    public boolean isVegan(){ return this.vegan;}
+    public boolean isVegan(){ return this.vegan; }
 
     /**
      * Returns true if course is vegetarian. False otherwise.
      * @return true or false
      */
-    public boolean isVegetarian(){ return this.vegetarian;}
+    public boolean isVegetarian(){ return this.vegetarian; }
 
     /**
      * Returns true if course is spicy. False otherwise.
      * @return true or false
      */
-    public boolean isSpicy(){ return this.spicy;}
+    public boolean isSpicy(){ return this.spicy; }
 
     /**
      * Sets the Course ID
      * @param cid Course ID
+     * @throws CourseException if Course ID is negative.
      */
-    public void setCid(int cid){ this.cid = cid;}
+    public void setCid(int cid) throws CourseException {
+        if(cid < 0)
+            throw new CourseException("Course ID must be positive");
+        this.cid = cid;
+    }
 
-    public void setMid(int mid){ this.mid = mid; }
+    /**
+     * Sets the Menu ID of the Menu whose this course belongs to.
+     * @param mid Menu ID
+     * @throws CourseException if Menu ID is negative.
+     */
+    public void setMid(int mid) throws CourseException {
+        if(mid < 0)
+            throw new CourseException("Menu ID must be positive");
+        this.mid = mid;
+    }
 
     /**
      * Sets the name of the course
@@ -166,6 +209,12 @@ public class Course {
      * @param image Uri of the image.
      */
     public void setImageUri(Uri image){ this.image = image;}
+
+    /**
+     *
+     * @param uri A string representing the Uri of the image
+     */
+    public void setImageUriFromString(String uri){ this.image = Uri.parse(uri); }
 
     /**
      * Sets if the course is gluten-free
