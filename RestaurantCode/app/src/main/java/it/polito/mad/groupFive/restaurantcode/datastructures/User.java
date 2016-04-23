45a36c45394a10dc4d48e4082cc5e74eb48c225d
Marco Ardizzone <kwbmm1990@gmail.com>
2016-04-23 15:36:53 +0200

    Create User class and its subclasses (RestaurantOwner and Customer). These classes can be persisted to JSON

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/User.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/User.java
@@ -1,71 +1,206 @@
 package it.polito.mad.groupFive.restaurantcode.datastructures;
 
 import android.content.Context;
-import android.util.Log;
+import android.net.Uri;
+import android.os.Build;
 
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-import it.polito.mad.groupFive.restaurantcode.datastructures.exceptions.RestaurantException;
 import it.polito.mad.groupFive.restaurantcode.datastructures.exceptions.UserException;
 
 /**
- * @author Giovanni
+ * @author Marco Ardizzone
  * @class User
- * @date 04/04/16
+ * @date 2016-04-23
  * @brief User class
  *
  */
-public class User {
-    private int uid;
-    private Restaurant restaurant;
-    private String name;
-    private String surname;
-    private String address;
-    private byte[] image;
-    private String nickname;
-    private ArrayList<Restaurant> favorites;
-    private boolean restaurantOwner;
+public abstract class User {
+
+    transient protected Context appContext;
+
+    protected int uid;
+    protected String name;
+    protected String surname;
+    protected String userName;
+    protected String password;
+    protected String address;
+    protected Uri image;
+    protected ArrayList<Review> reviews = new ArrayList<>();
 
     /**
-     * Create a User object. Requires, as parameters, the Android Application Context of the
-     * activity instantiating this class, a positive integer uniquely identifying the restaurant
-     * object and another positive integer uniquely identifying the user object.
+     * Generate a random integer in the range [1, Integer.MAX_VALUE]
+     * @return In integer in the range [1, Integer.MAX_VALUE]
+     */
+    protected static int randInt() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            return ThreadLocalRandom.current().nextInt(1,Integer.MAX_VALUE);
+        else{
+            Random rand= new Random();
+            int result;
+            if((result=rand.nextInt(Integer.MAX_VALUE)) == 0)
+                return Restaurant.randInt();
+            return result;
+        }
+    }
+
+    /**
+     * Reads the JSON file corresponding to this user and fills this class with the data found
+     * in the file.
+     * If the file doesn't exist CreateJSONFile is called and the file is created. Then the reading
+     * is performed again: this time the file will be found and fields of this class will be filled
+     * with null values (because the created file is empty).
+     *
+     * If a fail occurs, the error message is logged and this method returns null.
+     *
+     * @return A User object or null if fails.
+     */
+    protected abstract User readJSONFile();
+
+    /**
+     * Create the JSON file corresponding to this object. The JSON file is identified by the
+     * user ID set at instantiation time.
+     * If an error occurs, the error is logged.
+     */
+    protected abstract void createJSONFile();
+
+    /**
+     * Copy all the data took from the JSON file on this object.
+     * @param dummy A dummy User object, on which the JSON data is written to.
+     */
+    protected abstract void copyData(User dummy);
+
+    /**
+     * Saves the current data store in this object to its JSON file.
+     * Note that this method also saves the data for the custom sub-objects embedded inside this
+     * class (Review).
      * In case of fail, the error is logged and a UserException is thrown.
      *
-     * @param c The Android Application Context
-     * @param rid A positive unique integer identifying a restaurant object
-     * @param uid A positive unique integer identifying a user object
-     * @throws UserException If user id is negative or if restaurant creation fails for some reason.
+     * @throws UserException If writing JSON file fails
      */
-    public User(Context c, int rid, int uid) throws UserException {
-        final String METHOD_NAME = this.getClass().getName()+" - constructor";
+    protected abstract void saveData() throws UserException;
+
+    /**
+     * Fetch the data corresponding to the User ID of this object from the JSON file.
+     * This method is in charge of filling all the other classes of this data set (Review).
+     * If an error occurs, it is logged and a UserException is thrown.
+     *
+     * @throws UserException If fetch fails
+     */
+    protected abstract void getData() throws UserException;
+
+    /**
+     *
+     * @return The unique identifier of this User object
+     */
+    public int getUid() {
+        return this.uid;
+    }
+
+    /**
+     *
+     * @return The name of user
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @return The surname of user
+     */
+    public String getSurname() {
+        return this.surname;
+    }
+
+    /**
+     *
+     * @return The address of user
+     */
+    public String getAddress() {
+        return this.address;
+    }
+
+    /**
+     *
+     * @return The userName of the user
+     */
+    public String getUserName() {
+        return this.userName;
+    }
+
+    /**
+     *
+     * @return The password of the user
+     */
+    public String getPassword(){ return this.password; }
+
+    /**
+     *
+     * @return Uri of user's image
+     */
+    public Uri getImageUri() {
+        return this.image;
+    }
+
+    /**
+     *
+     * @return An ArrayList of reviews belonging to this user
+     */
+    public ArrayList<Review> getReviews(){ return this.reviews; }
+
+    /**
+     * Get the Review object corresponding to the supplied ID.
+     * If no Review object is matching the input id, this method returns null.
+     * An exception is thrown in case of negative review id.
+     *
+     * @param revID A positive integer uniquely identifying the review object
+     * @return A review object matching the input id, or null if nothing is found.
+     * @throws UserException If review id is negative
+     */
+    public Review getReviewByID(int revID) throws UserException {
+        if(revID < 0)
+            throw new UserException("Review ID must be positive");
+        for(Review r : this.reviews)
+            if(r.getRevID() == revID)
+                return r;
+        return null;
+    }
+
+    /**
+     * Get the ArrayList of Review objects made by this user matching the supplied restaurant ID.
+     * If no Review matches the input restaurant ID, an empty ArrayList is returned
+     *
+     * @param rid A positive integer uniquely identifying a restaurant.
+     * @return An ArrayList of Review objects
+     * @throws UserException If restaurant id is negative.
+     */
+    public ArrayList<Review> getReviewsByRestaurantID(int rid) throws UserException {
+        if(rid < 0)
+            throw new UserException("Restaurant ID must be positive");
+        ArrayList<Review> returnRes = new ArrayList<>();
+        for(Review r : this.reviews)
+            if(r.getRid() == rid)
+                returnRes.add(r);
+        return returnRes;
+    }
+
+    /**
+     *
+     * @param uid Positive integer uniquely identifying the user
+     * @throws UserException If user id is negative
+     */
+    public void setUid(int uid) throws UserException {
         if(uid < 0)
             throw new UserException("User ID must be positive");
         this.uid = uid;
-
-        try {
-            this.restaurant = new Restaurant(c,rid);
-        } catch (RestaurantException e) {
-            Log.e(METHOD_NAME,e.getMessage());
-        }
-        this.restaurantOwner = true;
     }
 
     /**
      *
-     * @return the name of user
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     *
-     * @param name of the user
+     * @param name Name of the user
      */
     public void setName(String name) {
         this.name = name;
@@ -73,15 +208,7 @@ public class User {
 
     /**
      *
-     * @return the surname of user
-     */
-    public String getSurname() {
-        return surname;
-    }
-
-    /**
-     *
-     * @param surname of the user
+     * @param surname Surname of the user
      */
     public void setSurname(String surname) {
         this.surname = surname;
@@ -89,15 +216,7 @@ public class User {
 
     /**
      *
-     * @return the address of user
-     */
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     *
-     * @param address of the user
+     * @param address Address of the user
      */
     public void setAddress(String address) {
         this.address = address;
@@ -105,96 +224,32 @@ public class User {
 
     /**
      *
-     * @return the nickname of the user
+     * @param userName of the user
      */
-    public String getNickname() {
-        return nickname;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     /**
      *
-     * @param nickname of the user
+     * @param password Password of the user
      */
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
+    public void setPassword(String password){
+        //TODO Implement some encryption for password - Sad LIOY is sad;
+        this.password = password;
     }
 
     /**
      *
-     * @return byte[] of user's image
+     * @param image Uri of the user image
      */
-    public byte[] getImage() {
-        return image;
-    }
-
-    /**
-     *
-     * @param image of user
-     */
-    public void setImage(byte[] image) {
+    public void setImageUri(Uri image) {
         this.image = image;
     }
 
     /**
      *
-     * @return yes if the user is the restaurant owner
+     * @param r ArrayList of reviews made by the user.
      */
-    public boolean isRestaurantowner() {
-        return this.restaurantOwner;
-    }
-
-    /**
-     *
-     * @param restaurantowner if is or is not a restaurant owner
-     */
-    public void setRestaurantowner(boolean restaurantowner) { this.restaurantOwner = restaurantowner;}
-
-    /**
-     *
-     * @return the id of user
-     */
-    public int getUid() {
-        return uid;
-    }
-
-    /**
-     *
-     * @param uid the id of user
-     */
-    public void setUid(int uid) {
-        this.uid = uid;
-    }
-
-    /**
-     *
-     * @return the arraylist of user's favorite restaurants 
-     */
-    public ArrayList<Restaurant> getFavorites() {
-        return favorites;
-    }
-
-    /**
-     *
-     * @param favorite: the arraylist of user's favorite restaurants
-     */
-    public void setFavorites(ArrayList<Restaurant> favorite) {
-        this.favorites = favorite;
-    }
-
-    /**
-     *
-     * @return the restaurant of the user (restaurant manager)
-     */
-    public Restaurant getRestaurant() {
-        return restaurant;
-    }
-
-    /**
-     *
-     * @param restaurant: the restaurant of the user (restaurant manager)
-     */
-    public void setRestaurant(Restaurant restaurant) {
-        this.restaurant = restaurant;
-    }
-
+    public void setReviews(ArrayList<Review> r){ this.reviews = r; }
 }
--- TODO.md
+++ TODO.md
@@ -1,9 +1,4 @@
-review class
-	string comment
-	arraylist reply
-	float rating
-
-restaurant and user add arraylist review
+user add arraylist review
 
 all classes getimagebitmap
 
