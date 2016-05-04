a1eb1f8393d7cc0ae85df085a3ae3769c70ce8b8
Cristiano Ovio <creeovio@gmail.com>
2016-05-04 23:48:48 +0200

    removed fake data creation from Menu_details

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Menu_details.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/Menu_details.java
@@ -43,7 +43,7 @@ public class Menu_details extends NavigationDrawer {
         try {
             restaurant=new Restaurant(getBaseContext(),rid);
             restaurant.getData();
-            generamenufittizio();
+            //generamenufittizio();
             menu=restaurant.getMenuByID(mid);
         } catch (RestaurantException e) {
             e.printStackTrace();
