d1d25096d26fb1aff258dacd55074087c920a507
Marco Ardizzone <kwbmm1990@gmail.com>
2016-05-08 17:34:59 +0200

    Adjust checkbox for lunch/dinner timetable: now buttons with time are shown only if checkbox is checked.

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/CreateRestaurant/CreateRestaurant_4.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/CreateRestaurant/CreateRestaurant_4.java
@@ -98,9 +98,26 @@ public class CreateRestaurant_4 extends Fragment {
 
         for(String weekday : this.weekDays){
             LayoutInflater li = LayoutInflater.from(this.parentView.getContext());
-            View timetableItem = li.inflate(R.layout.timetable_item,null);
-            ((CheckBox)timetableItem.findViewById(R.id.checkBox)).setText(weekday);
-
+            final View timetableItem = li.inflate(R.layout.timetable_item,null);
+            CheckBox cb = (CheckBox)timetableItem.findViewById(R.id.checkBox);
+            cb.setText(weekday);
+            cb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(((CheckBox)v).isChecked()){
+                        timetableItem.findViewById(R.id.from).setVisibility(View.VISIBLE);
+                        timetableItem.findViewById(R.id.textClockFrom).setVisibility(View.VISIBLE);
+                        timetableItem.findViewById(R.id.to).setVisibility(View.VISIBLE);
+                        timetableItem.findViewById(R.id.textClockTo).setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        timetableItem.findViewById(R.id.from).setVisibility(View.INVISIBLE);
+                        timetableItem.findViewById(R.id.textClockFrom).setVisibility(View.INVISIBLE);
+                        timetableItem.findViewById(R.id.to).setVisibility(View.INVISIBLE);
+                        timetableItem.findViewById(R.id.textClockTo).setVisibility(View.INVISIBLE);
+                    }
+                }
+            });
             weekdayToRL_IDs.put(weekday,Restaurant.randInt());
 
             //Set the clock popup for both buttons (to and from)
