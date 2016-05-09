44da93286f0ec84146e68b3f847449b56b97fe53
Federico Merlin <federico.merlin@gmail.com>
2016-05-09 18:27:45 +0200

    Create Simple menu improvements

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/CreateSimpleMenu/Add_simple_dish.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/CreateSimpleMenu/Add_simple_dish.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -149,6 +151,10 @@ public class Add_simple_dish extends Fragment{
 
             }
         });
+        Spinner cuisine = (Spinner) v.findViewById(R.id.nd_type);
+        cuisine.setAdapter(new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_dropdown_item,getResources().getStringArray(R.array.cuisine)));
+        Spinner type = (Spinner) v.findViewById(R.id.nd_type_2);
+        type.setAdapter(new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_dropdown_item,getResources().getStringArray(R.array.type)));
         view=v;
         return v;
     }
