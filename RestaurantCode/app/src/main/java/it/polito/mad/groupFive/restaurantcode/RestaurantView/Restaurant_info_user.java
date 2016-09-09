af9921fc7bc13dfaf5e4bdfe86a7659429d6225a
Cristiano Ovio <creeovio@gmail.com>
2016-09-09 14:27:06 +0200

    "Add to favourites" button in RestView now works

--- RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/RestaurantView/Restaurant_info_user.java
+++ RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/RestaurantView/Restaurant_info_user.java
@@ -15,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,12 +25,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import it.polito.mad.groupFive.restaurantcode.R;
 import it.polito.mad.groupFive.restaurantcode.datastructures.Picture;
@@ -46,14 +55,20 @@ import it.polito.mad.groupFive.restaurantcode.datastructures.Restaurant;
  */
 public class Restaurant_info_user extends Fragment implements OnMapReadyCallback {
 
-    public interface restaurantData{
+    public interface restaurantData {
         public Restaurant getRestaurant();
     }
+
     restaurantData data;
     Restaurant restaurant;
+    ArrayList<String> favourites;
+    String uid;
+    Boolean auth = false;
     MapView mapView;
     FileDownloadTask imageDownloadTask;
     ImageDownloadListener idl;
+    ImageView favicon;
+    TextView favtext;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -102,85 +117,96 @@ public class Restaurant_info_user extends Fragment implements OnMapReadyCallback
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View v=inflater.inflate(R.layout.fragment_restaurant_info_user, container, false);
+        View v = inflater.inflate(R.layout.fragment_restaurant_info_user, container, false);
         getRestaurantData(v);
         //init map
         MapsInitializer.initialize(this.getActivity());
         mapView = (CustomMapView) v.findViewById(R.id.gmap);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+        //init favourites
+        favourites = new ArrayList<>();
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference("favourite");
+        FirebaseUser usr = FirebaseAuth.getInstance().getCurrentUser();
+        if (usr != null) {
+            uid = usr.getUid();
+            auth = true;
+            ref.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Iterable<DataSnapshot> favs = dataSnapshot.getChildren();
+                    for (DataSnapshot d : favs) {
+                        favourites.add(d.getKey());
+                    }
+                    if (favourites.contains(restaurant.getRid())) {
+                        favicon.setImageDrawable(getResources().getDrawable(R.drawable.ic_heart_filled_black));
+                        favtext.setText(R.string.unfav);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
         return v;
     }
 
     public void getRestaurantData(View v) {
-        restaurant=data.getRestaurant();
-        TextView restopen= (TextView)v.findViewById(R.id.restaurant_open);
-        ImageView open=(ImageView)v.findViewById(R.id.imageView9);
-        if(restaurant.isOpen()){
+        restaurant = data.getRestaurant();
+        TextView restopen = (TextView) v.findViewById(R.id.restaurant_open);
+        ImageView open = (ImageView) v.findViewById(R.id.imageView9);
+        if (restaurant.isOpen()) {
             restopen.setText(R.string.nowOpen);
-            restopen.setTextColor(Color.rgb(0,100,0));
-        }
-        else {
+            restopen.setTextColor(Color.rgb(0, 100, 0));
+        } else {
             restopen.setText(R.string.nowClosed);
-            restopen.setTextColor(Color.rgb(200,0,0));
+            restopen.setTextColor(Color.rgb(200, 0, 0));
         }
-        TextView restdescr = (TextView)v.findViewById(R.id.restaurant_description);
+        TextView restdescr = (TextView) v.findViewById(R.id.restaurant_description);
         restdescr.setText(restaurant.getDescription());
-        TextView restaddr=(TextView) v.findViewById(R.id.restaurant_address);
-        restaddr.setText(restaurant.getAddress()+", "+restaurant.getCity()+" "+restaurant.getZip()+" "+restaurant.getState());
-        RelativeLayout resttel=(RelativeLayout)v.findViewById(R.id.layout_call);
+        TextView restaddr = (TextView) v.findViewById(R.id.restaurant_address);
+        restaddr.setText(restaurant.getAddress() + ", " + restaurant.getCity() + " " + restaurant.getZip() + " " + restaurant.getState());
+        RelativeLayout resttel = (RelativeLayout) v.findViewById(R.id.layout_call);
         resttel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent dialIntent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:"+restaurant.getTelephone()));
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + restaurant.getTelephone()));
                 startActivity(dialIntent);
             }
         });
-        RelativeLayout restweb=(RelativeLayout)v.findViewById(R.id.layout_web);
+        RelativeLayout restweb = (RelativeLayout) v.findViewById(R.id.layout_web);
         restweb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent webIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://"+restaurant.getWebsite()));
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + restaurant.getWebsite()));
                 startActivity(webIntent);
             }
         });
-        RelativeLayout restfav=(RelativeLayout)v.findViewById(R.id.layout_fav);
-        restfav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO
-            }
+        favicon = (ImageView) v.findViewById(R.id.favIcon);
+        favtext = (TextView) v.findViewById(R.id.favtext);
+        RelativeLayout restfav = (RelativeLayout) v.findViewById(R.id.layout_fav);
+        restfav.setOnClickListener(new OnFavourite(restaurant));
 
-        });
-
-        /*
-        ImageView restImage=(ImageView)v.findViewById(R.id.restaurant_image);
-
-        FirebaseStorage storage=FirebaseStorage.getInstance();
-        StorageReference imageref=storage.getReferenceFromUrl("gs://luminous-heat-4574.appspot.com/restaurant/");
-        try {
-            getFromNetwork(imageref,restaurant.getRid(),restImage);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        */
         final LinearLayout ll = (LinearLayout) v.findViewById(R.id.restaurant_time_t);
-        int count=0;
-        for(String weekday : getResources().getStringArray(R.array.weekENG)){
+        int count = 0;
+        for (String weekday : getResources().getStringArray(R.array.weekENG)) {
             LayoutInflater li = LayoutInflater.from(v.getContext());
-            View timetableItem = li.inflate(R.layout.timetable_view,null);
-            TextView dow= (TextView) timetableItem.findViewById(R.id.dow);
+            View timetableItem = li.inflate(R.layout.timetable_view, null);
+            TextView dow = (TextView) timetableItem.findViewById(R.id.dow);
             dow.setText(getResources().getStringArray(R.array.week)[count]);
-            String time="";
-            TextView lunch=(TextView)timetableItem.findViewById(R.id.time_lunch);
-            if (restaurant.getTimetableLunch()!=null&&restaurant.getTimetableLunch().containsKey(weekday)) {
-                time=time.concat(restaurant.getTimetableLunch().get(weekday).get("start")+"-"+restaurant.getTimetableLunch().get(weekday).get("end"));
+            String time = "";
+            TextView lunch = (TextView) timetableItem.findViewById(R.id.time_lunch);
+            if (restaurant.getTimetableLunch() != null && restaurant.getTimetableLunch().containsKey(weekday)) {
+                time = time.concat(restaurant.getTimetableLunch().get(weekday).get("start") + "-" + restaurant.getTimetableLunch().get(weekday).get("end"));
             }
-            if (restaurant.getTimetableDinner()!=null&&restaurant.getTimetableDinner().containsKey(weekday)) {
-                if(!time.equals("")) time=time.concat("\n");
-                time=time.concat((restaurant.getTimetableDinner().get(weekday).get("start") + "-" + (restaurant.getTimetableDinner().get(weekday).get("end"))));
+            if (restaurant.getTimetableDinner() != null && restaurant.getTimetableDinner().containsKey(weekday)) {
+                if (!time.equals("")) time = time.concat("\n");
+                time = time.concat((restaurant.getTimetableDinner().get(weekday).get("start") + "-" + (restaurant.getTimetableDinner().get(weekday).get("end"))));
             }
-            if(time.equals("")) time=getString(R.string.closed);
+            if (time.equals("")) time = getString(R.string.closed);
             lunch.setText(time);
             ll.addView(timetableItem);
             count++;
@@ -192,7 +218,7 @@ public class Restaurant_info_user extends Fragment implements OnMapReadyCallback
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
-            data=(restaurantData) context;
+            data = (restaurantData) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -219,49 +245,50 @@ public class Restaurant_info_user extends Fragment implements OnMapReadyCallback
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
     private void getFromNetwork(StorageReference storageRoot, final String id, final ImageView imView) throws FileNotFoundException {
         ContextWrapper cw = new ContextWrapper(getActivity().getApplicationContext());
         final File dir = cw.getDir("images", Context.MODE_PRIVATE);
-        File filePath = new File(dir,id);
+        File filePath = new File(dir, id);
         this.imageDownloadTask = storageRoot.child(id).getFile(filePath);
-        this.idl = new ImageDownloadListener(imView,dir,id);
+        this.idl = new ImageDownloadListener(imView, dir, id);
         this.imageDownloadTask.addOnSuccessListener(this.idl);
     }
 
     @Override
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         //TODO this.imageDownloadTask.removeOnSuccessListener(this.idl);
         mapView.onPause();
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState){
+    public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
 
     @Override
-    public void onLowMemory(){
+    public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         mapView.onResume();
     }
 
     @Override
-    public void onMapReady(GoogleMap map){
-        LatLng latLng = new LatLng(restaurant.getXCoord(),restaurant.getYCoord());
+    public void onMapReady(GoogleMap map) {
+        LatLng latLng = new LatLng(restaurant.getXCoord(), restaurant.getYCoord());
         map.addMarker(new MarkerOptions()
                 .position(latLng)
                 .title(restaurant.getName()));
@@ -269,24 +296,89 @@ public class Restaurant_info_user extends Fragment implements OnMapReadyCallback
         map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 
-    private class ImageDownloadListener implements OnSuccessListener<FileDownloadTask.TaskSnapshot>{
+    private class ImageDownloadListener implements OnSuccessListener<FileDownloadTask.TaskSnapshot> {
         private ImageView imView;
         private File directory;
         private String id;
-        public ImageDownloadListener(ImageView imageView, File imageDirectory, String imageID){
+
+        public ImageDownloadListener(ImageView imageView, File imageDirectory, String imageID) {
             this.imView = imageView;
             this.directory = imageDirectory;
             this.id = imageID;
         }
+
         @Override
         public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
             File img = new File(this.directory, this.id);
             Uri imgPath = Uri.fromFile(img);
             try {
-                Bitmap b = new Picture(imgPath,getActivity().getContentResolver()).getBitmap();
+                Bitmap b = new Picture(imgPath, getActivity().getContentResolver()).getBitmap();
                 this.imView.setImageBitmap(b);
             } catch (IOException e) {
                 //Log.e("getFromNet",e.getMessage());
+            }
+        }
+    }
+
+    public class OnFavourite implements View.OnClickListener {
+        private Restaurant restaurant;
+
+        public OnFavourite(Restaurant restaurant) {
+            this.restaurant = restaurant;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (auth == true) {
+                if (favourites.contains(restaurant.getRid())) {
+                    //is favourite
+                    favourites.remove(restaurant.getRid());
+                    favicon.setImageDrawable(getResources().getDrawable(R.drawable.ic_heart_border_black));
+                    favtext.setText(R.string.fav);
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    DatabaseReference ref = db.getReference("favourite");
+                    ref.child(uid).child(restaurant.getRid()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                        }
+                    });
+                    //remove on server
+                } else {
+                    favourites.add(restaurant.getRid());
+                    //add on server
+                    favicon.setImageDrawable(getResources().getDrawable(R.drawable.ic_heart_filled_black));
+                    favtext.setText(R.string.unfav);
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    final DatabaseReference menus = db.getReference("menu");
+                    menus.orderByChild("rid").equalTo(restaurant.getRid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            FirebaseDatabase db = FirebaseDatabase.getInstance();
+                            DatabaseReference ref = db.getReference("favourite");
+                            Iterable<DataSnapshot> favs = dataSnapshot.getChildren();
+                            for (DataSnapshot d : favs) {
+
+                                ref.child(uid).child(restaurant.getRid()).child(d.getKey()).setValue(true).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                    }
+                                });
+
+
+                            }
+                            Toast.makeText(getContext(), restaurant.getName() + getResources().getString(R.string.added_favourite), Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+
+                }
+
+            }
+            else{
+                Toast.makeText(getContext(), getResources().getString(R.string.need_to_login), Toast.LENGTH_LONG).show();
             }
         }
     }
