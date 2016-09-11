Kwbmm/UniversEat

commit ea8f1f104fe01335683054d25e80d71985081867

Issue #41: Reviews are now ordered by most recent first. However it should be tested as some strange behaviour is going on and I'm not sure if it is supposed to work like that.. If you try to review more than once then the last review is overwritten. This could be due to the SortedList calls to areItemsTheSame and areContentsTheSame.

RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/RestaurantView/Review_user_view.java
@@ -6,8 +6,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.util.SortedList;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,7 +25,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import it.polito.mad.groupFive.restaurantcode.R;
 import it.polito.mad.groupFive.restaurantcode.datastructures.Restaurant;
@@ -79,21 +85,14 @@ public class Review_user_view extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         readdata(getView());
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-
-    }
-
-    public void readdata(View v) {
-
-
+    private void readdata(View v) {
         adp= new ReviewAdapter();
         recyclerView=(RecyclerView)v.findViewById(R.id.review_list);
         recyclerView.setAdapter(adp);
@@ -110,7 +109,7 @@ public class Review_user_view extends Fragment {
                 Bundle bundle = new Bundle();
                 bundle.putString("rid",rest.getRid());
                 bundle.putFloat("ratingNumber",rest.getRatingNumber());
-                bundle.putFloat("ratingValue",rest.getRating());
+                bundle.putFloat("ratingValue",rest.getRating()*rest.getRatingNumber());
                 New_Create_review create_review = new New_Create_review();
                 create_review.setArguments(bundle);
                 getActivity().getSupportFragmentManager().beginTransaction()
@@ -118,13 +117,11 @@ public class Review_user_view extends Fragment {
                         .addToBackStack(null).add(R.id.frame,create_review).commit();
             }
         });
+    }
 
-        /*TextView r_count= (TextView)v.findViewById(R.id.review_counter);
-        r_count.setText(((int)rest.getRatingNumber())+" reviews");
-        TextView r_name=(TextView)v.findViewById(R.id.restaurant_name);
-        r_name.setText(rest.getName());
-        RatingBar rate=(RatingBar)v.findViewById(R.id.rate);
-        rate.setRating(rest.getRating());*/
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
 
     }
 
@@ -141,19 +138,18 @@ public class Review_user_view extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        reviews=new ArrayList<>();
+
         rest=restData.getRestaurant();
         View v =inflater.inflate(R.layout.fragment_review_user_view, container, false);
+        readdata(v);
+
         FirebaseDatabase db=FirebaseDatabase.getInstance();
         DatabaseReference reference=db.getReference("review");
-        //Log.v("rev id ",rest.getRid());
         reference.orderByChild("rid").equalTo(rest.getRid()).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Review nrev=dataSnapshot.getValue(Review.class);
-                reviews.add(nrev);
-                adp.notifyDataSetChanged();
+                adp.addChild(nrev);
             }
 
             @Override
@@ -176,8 +172,6 @@ public class Review_user_view extends Fragment {
 
             }
         });
-        readdata(v);
-
         return v;
     }
 
@@ -218,16 +212,68 @@ public class Review_user_view extends Fragment {
 
     public class ReviewAdapter extends RecyclerView.Adapter<ReviewHolder> {
 
+        private SortedList<Review> reviews;
+
         public ReviewAdapter(){
+            this.reviews = new SortedList<Review>(Review.class, new SortedList.Callback<Review>() {
+                @Override
+                public int compare(Review o1, Review o2) {
+                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                    try {
+                        Date d1 = df.parse(o1.getDate());
+                        Date d2 = df.parse(o2.getDate());
+                        if(d1.getTime() < d2.getTime())
+                            return 1;
+                        if(d1.getTime() > d2.getTime())
+                            return -1;
+                        else
+                            return 0;
+                    } catch (ParseException e) {
+                        Log.e("Compare",e.getMessage());
+                    }
+                    return 0;
+                }
 
+                @Override
+                public void onInserted(int position, int count) {
+                    notifyItemRangeInserted(position,count);
+                }
 
+                @Override
+                public void onRemoved(int position, int count) {
+                    notifyItemRangeRemoved(position,count);
+                }
+
+                @Override
+                public void onMoved(int fromPosition, int toPosition) {
+                    notifyItemMoved(fromPosition,toPosition);
+                }
+
+                @Override
+                public void onChanged(int position, int count) {
+                    notifyItemRangeChanged(position,count);
+                }
+
+                @Override
+                public boolean areContentsTheSame(Review oldItem, Review newItem) {
+                    return oldItem.getRid().equals(newItem.getRid()) && oldItem.getDate().equals(newItem.getDate()) && oldItem.getUid().equals(newItem.getUid());
+                }
+
+                @Override
+                public boolean areItemsTheSame(Review item1, Review item2) {
+                    return item1.getRid().equals(item2.getRid()) && item1.getDate().equals(item2.getDate()) && item1.getUid().equals(item2.getUid());
+                }
+            });
+        }
+
+        public void addChild(Review r){
+            this.reviews.add(r);
         }
 
         @Override
         public ReviewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View review_view = LayoutInflater.from(parent.getContext()).inflate(R.layout.review,null);
             ReviewHolder holder=new ReviewHolder(review_view);
-
             return holder;
         }
 
@@ -238,7 +284,6 @@ public class Review_user_view extends Fragment {
             holder.title.setText(rev.getTitle());
             holder.review.setText("                         "+rev.getReviewText());
             holder.dat.setText(rev.getDate());
-
         }
 
         @Override

RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/SearchRestaurants/RestaurantAdapter.java
@@ -274,7 +274,6 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantViewHolder
         if(restaurant.getName().length()>18)
             holder.restaurant_name.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
         holder.restaurant_rating.setRating(restaurant.getRating()*restaurant.getRatingNumber());
-        Log.d("Fix Rating","rating: "+holder.restaurant_rating.getRating());
         String s;
         holder.restaurant_description.setText(restaurant.getDescription());
         float distance = restaurant.getDistance();

commit f6b5e665e7f948b791f3a3611d7a3e7826a45caa

the average ratings are FINALLY working. the bug introduced in the previous commit (ea8f1f104..) is fixed.

RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/RestaurantView/New_Create_review.java
@@ -113,7 +113,10 @@ public class New_Create_review extends Fragment {
                 int day=date.get(Calendar.DAY_OF_MONTH);
                 int month=date.get(Calendar.MONTH)+1;
                 int year=date.get(Calendar.YEAR);
-                rev.setDate(day + "/" + month + "/" + year);
+                int hour=date.get(Calendar.HOUR_OF_DAY);
+                int minutes=date.get(Calendar.MINUTE);
+                int seconds=date.get(Calendar.SECOND);
+                rev.setDate(day + "/" + month + "/" + year + " " + String.format("%02d:%02d:%02d", hour,minutes,seconds));
                 DatabaseReference ref = db.getReference("review");
                 ref.push().setValue(rev).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
@@ -126,7 +129,7 @@ public class New_Create_review extends Fragment {
                             public void onComplete(@NonNull Task<Void> task) {
                                 FirebaseDatabase db = FirebaseDatabase.getInstance();
                                 DatabaseReference reference = db.getReference("restaurant");
-                                Float tot = ratingValue + sum;
+                                Float tot = (ratingValue + sum)/ratingNumber;
                                 reference.child(rid).child("rating").setValue(tot.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
                                     @Override
                                     public void onComplete(@NonNull Task<Void> task) {

RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/RestaurantView/Review_user_view.java
@@ -218,7 +218,7 @@ public class Review_user_view extends Fragment {
             this.reviews = new SortedList<Review>(Review.class, new SortedList.Callback<Review>() {
                 @Override
                 public int compare(Review o1, Review o2) {
-                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss");
                     try {
                         Date d1 = df.parse(o1.getDate());
                         Date d2 = df.parse(o2.getDate());
@@ -283,7 +283,8 @@ public class Review_user_view extends Fragment {
             holder.rating.setRating(rev.getRating());
             holder.title.setText(rev.getTitle());
             holder.review.setText("                         "+rev.getReviewText());
-            holder.dat.setText(rev.getDate());
+            String[] date=rev.getDate().split(" ");
+            holder.dat.setText(date[0]);
         }
 
         @Override

RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/RestaurantView/User_info_view.java
@@ -101,8 +101,8 @@ public class User_info_view extends NavigationDrawer implements Restaurant_info_
                     r_name.setText(restaurant.getName());
                     if(restaurant.getName().length()>18)
                         r_name.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
-                    r_rating.setText(String.format("%.1f", restaurant.getRating()*restaurant.getRatingNumber()));
-                    r_ratingBar.setRating(restaurant.getRating()*restaurant.getRatingNumber());
+                    r_rating.setText(String.format("%.1f", restaurant.getRating()));
+                    r_ratingBar.setRating(restaurant.getRating());
                     r_reviews.setText(String.format("%.0f", restaurant.getRatingNumber()) + " " + getResources().getString(R.string.review_based_on));
                     ImageView restImage = (ImageView) findViewById(R.id.restaurant_image);
 

RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/SearchRestaurants/RestaurantAdapter.java
@@ -50,7 +50,7 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantViewHolder
                     .setImageLocalPath(r.getImageLocalPath())
                     .setAddress(r.getAddress())
                     .setCity(r.getCity())
-                    .setRating(r.getRating()*r.getRatingNumber())
+                    .setRating(r.getRating())
                     .setRatingNumber(r.getRatingNumber())
                     .setState(r.getState())
                     .setXCoord(r.getXCoord())
@@ -273,7 +273,7 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantViewHolder
         holder.restaurant_name.setText(restaurant.getName());
         if(restaurant.getName().length()>18)
             holder.restaurant_name.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
-        holder.restaurant_rating.setRating(restaurant.getRating()*restaurant.getRatingNumber());
+        holder.restaurant_rating.setRating(restaurant.getRating());
         String s;
         holder.restaurant_description.setText(restaurant.getDescription());
         float distance = restaurant.getDistance();

RestaurantCode/app/src/main/java/it/polito/mad/groupFive/restaurantcode/datastructures/Restaurant.java
@@ -167,7 +167,7 @@ public class Restaurant {
      * @return the rating of restaurant
      */
     public float getRating() {
-        return rating/ratingNumber;}
+        return rating;}
 
     /**
      *

RestaurantCode/app/src/main/res/values-it/strings.xml
@@ -434,7 +434,7 @@
     <string name="add_picture">Scegli una foto...</string>
     <string name="actionBar_addDish">Aggiungi un Piatto</string>
     <string name="actionBar_addTags">Aggiungi Tag</string>
-    <string name="view_restaurant">Vai al ristorante →</string>
+    <string name="view_restaurant">Vai al ristorante ></string>
     <string name="add_a_dish">Aggiungi un Piatto</string>
     <string name="Menu_view_edit_View">Vedi</string>
     <string name="Menu_view_edit_Editmenus">I miei menu</string>

RestaurantCode/app/src/main/res/values/strings.xml
@@ -493,7 +493,7 @@
     <string name="add_picture">Add a picture...</string>
     <string name="actionBar_addDish">Add a Dish</string>
     <string name="actionBar_addTags">Add Tags</string>
-    <string name="view_restaurant">View restaurant →</string>
+    <string name="view_restaurant">View restaurant ></string>
     <string name="add_a_dish">Add a Dish</string>
     <string name="Menu_view_edit_View">View</string>
     <string name="Menu_view_edit_Editmenus">My menus</string>